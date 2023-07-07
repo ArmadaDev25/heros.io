a5ec39e50b63b1651843e7e4651233a038d4ea34
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-07-06 21:53:43 -0700

    updated the generateHeroUI function so it displays the heroes name within the hero HTML element, also added parameters to the generateHeroUI function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // DEBUG to make sure the JS file is linked to the HTML fileS
 console.log('App is running and linked')
 
-
+generateHeroUI(100, "brad")
 
 class GameLogic {
     renown = 0
@@ -23,13 +23,21 @@ function generateHero(){
 
 
 // Creates a new HTML element that will hold the information on the newly created hero
-function generateHeroUI(){
+function generateHeroUI(health, name){
     const newElement = document.createElement("article") // Creates a new container element for the Hero information
     newElement.setAttribute("class", "hero-info") // Sets the hero containers class attribute to "hero-info"
+    // Creates the HTML element containing the heroes Name and adds it to the newElement containing the heroes health
+    const heroName = document.createElement("p")
+    const para2 = document.createTextNode(name)
+    heroName.appendChild(para2)
+    newElement.append(heroName)
+    // Creates the HTML element containing the heroes health and adds it to the newElement containing the heroes health
     const heroHealth = document.createElement("p")
-    const para = document.createTextNode("100")
-    heroHealth.appendChild(para)
+    const para1 = document.createTextNode(health)
+    heroHealth.appendChild(para1)
     newElement.appendChild(heroHealth)
+
+    // Gets the player-heroes element and makes the new HTML element containing the hero infomration a child of the player-heroes element
     const heroUISection = document.getElementById("player-heroes")
     heroUISection.appendChild(newElement)
     
