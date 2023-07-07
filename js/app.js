145be1a3614ade5c40fc11c8a97318bd142c52fc
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2023-07-06 21:34:57 -0700

    Created a function that creates a new HTML element that will contain hero info

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // DEBUG to make sure the JS file is linked to the HTML fileS
 console.log('App is running and linked')
 
+
+
 class GameLogic {
     renown = 0
     currency = 0
@@ -10,5 +12,19 @@ class Hero{
     constructor(name){
         this.name = name
     }
+    
+}
+
+
+// Creates a new HTML element that will hold the information on the newly created hero
+function generateHeroUI(){
+    const newElement = document.createElement("article") // Creates a new container element for the Hero information
+    newElement.setAttribute("class", "hero-info") // Sets the hero containers class attribute to "hero-info"
+    const heroHealth = document.createElement("p")
+    const para = document.createTextNode("100")
+    heroHealth.appendChild(para)
+    newElement.appendChild(heroHealth)
+    const heroUISection = document.getElementById("player-heroes")
+    heroUISection.appendChild(newElement)
     
 }
